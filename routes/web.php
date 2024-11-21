2a7228aa128ea54051c936ae824694678d8e36cb
majewskaana <a.majevska@inbox.lv>
2024-11-21 14:11:12 +0200

    bootstrap auth integration

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcomepage');
+    return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
